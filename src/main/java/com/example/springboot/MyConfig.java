1a19f40aff1724f52d50bb8def3223b53e14b190
lijun <869441079@qq.com>
2021-01-16 11:15:29 +0800

    删掉多的部分

--- src/main/java/com/example/springboot/MyConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot;
-
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-@EnableWebMvc
-public class MyConfig {
-}
--- src/main/java/com/example/springboot/Terst.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot;
-
-import org.apache.catalina.startup.Tomcat;
-
-public class Terst {
-    public static void main(String[] args) {
-        Tomcat t = new Tomcat();
-    }
-}
